c6fe5a8156dc5e459b3cc5774ca6b83cd3f62401
Matthew Sheydwasser <sheydwas@ualberta.ca>
2022-11-17 12:41:06 -0700

    endpoint testing

--- bot/return_tests.py
+++ bot/return_tests.py
@@ -5,8 +5,11 @@ def main():
     dets = endpoints.get_match_history_by_name_v3('na', 'Silas', '69LOL', game_mode='Competitive')
     
     # get map, game_length (do ((3.6 * (10^6)) * 60) to get minutes),
-    # game_start_patched = date of game, 
-    meta = dets[0].metadata
-    print(len(dets))
+    # game_start_patched = date of game, rounds played
+    
+    one = dets[0].players.blue[0]
+   
+    print(one)
+        
     
 main()
